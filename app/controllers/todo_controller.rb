4ad4a463012160a4a43cbcae54d0033bae4817bf
Tawhid Ali <tawhidali126@gmail.com>
2019-09-25 21:03:28 -0400

    created form_for

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,6 +1,33 @@
 class TodoController < ApplicationController
   def index
+    @todos = Todo.all
   end
 
-  
+  def show
+  end
+
+  def new
+    @todo = Todo.new
+    # form_for
+   
+  end
+
+  def create
+    # POST
+    # @todo.activity = params[:activity]
+    # @todo.done = params[:done]
+
+    item = Todo.create(params.require(:todo).permit(:activity, :done))
+    # item = Todo.create(input_params)
+    redirect_to todo_path(item)
+  end
+
+
+  # private
+
+  # def input_params
+  #   params.require(:todo).permit(:activity, :done)
+  # end
+
+
 end
